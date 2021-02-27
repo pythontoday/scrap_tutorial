73dbeed3aecea2a4d5ffbf8305c5e6eb50df5e83
pythontoday <pythontodaynet@gmail.com>
2021-02-27 16:17:10 +0700

    add lesson5

--- lesson4/test.py
+++ lesson4/test.py
@@ -1,4 +1,4 @@
 str1 = "Name: Whitby Pavilion"
 str2 = "Venue short url: https://www.skiddle.com/venues/545/"
-print(str1.split(":"))
-print(str2.split(":"))
+print(str1.split(":", 1))
+print(str2.split(":", 1))
---
+++ lesson5/main.py
@@ -0,0 +1,51 @@
+import os
+
+import requests
+import img2pdf
+
+
+def get_data():
+    headers = {
+        "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+    }
+
+    img_list = []
+    for i in range(1, 49):
+        url = f"https://www.recordpower.co.uk/flip/Winter2020/files/mobile/{i}.jpg"
+        req = requests.get(url=url, headers=headers)
+        response = req.content
+
+        with open(f"media/{i}.jpg", "wb") as file:
+            file.write(response)
+            img_list.append(f"media/{i}.jpg")
+            print(f"Downloaded {i} of 48")
+
+    print("#" * 20)
+    print(img_list)
+
+    # create PDF file
+    with open("result.pdf", "wb") as f:
+        f.write(img2pdf.convert(img_list))
+
+    print("PDF file created successfully!")
+
+
+def write_to_pdf():
+    # print(os.listdir("media"))
+    img_list = [f"media/{i}.jpg" for i in range(1, 49)]
+
+    # create PDF file
+    with open("result.pdf", "wb") as f:
+        f.write(img2pdf.convert(img_list))
+
+    print("PDF file created successfully!")
+
+
+def main():
+    # get_data()
+    write_to_pdf()
+
+
+if __name__ == '__main__':
+    main()
