047020e3a104eabff016ddb99f241700d5dd4172
pythontoday <pythontodaynet@gmail.com>
2021-11-07 18:56:29 +0700

    changed lesson13

--- lesson13/main.py
+++ lesson13/main.py
@@ -11,30 +11,32 @@ headers = {
 
 
 def get_articles_urls(url):
-    s = requests.Session()
-    response = s.get(url=url, headers=headers)
+    with requests.Session() as session:
+        response = session.get(url=url, headers=headers)
 
     soup = BeautifulSoup(response.text, 'lxml')
     pagination_count = int(soup.find('span', class_='navigations').find_all('a')[-1].text)
     
     articles_urls_list = []
-    for page in range(1, pagination_count + 1):
-    # for page in range(1, 2):
-        response = s.get(url=f'https://hi-tech.news/page/{page}/', headers=headers)
-        soup = BeautifulSoup(response.text, 'lxml')
+    
+    with requests.Session() as session:
+        for page in range(1, pagination_count + 1):
+        # for page in range(1, 2):
+            response = session.get(url=f'https://hi-tech.news/page/{page}/', headers=headers)
+            soup = BeautifulSoup(response.text, 'lxml')
 
-        articles_urls = soup.find_all('a', class_='post-title-a')
+            articles_urls = soup.find_all('a', class_='post-title-a')
 
-        for au in articles_urls:
-            art_url = au.get('href')
-            articles_urls_list.append(art_url)
+            for au in articles_urls:
+                art_url = au.get('href')
+                articles_urls_list.append(art_url)
 
-        # time.sleep(randrange(2, 5))
-        print(f'Обработал {page}/{pagination_count}')
+            # time.sleep(randrange(2, 5))
+            print(f'Обработал {page}/{pagination_count}')
 
-    with open('articles_urls.txt', 'w') as file:
-        for url in articles_urls_list:
-            file.write(f'{url}\n')
+        with open('articles_urls.txt', 'w') as file:
+            for url in articles_urls_list:
+                file.write(f'{url}\n')
 
     return 'Работа по сбору ссылок выполнена!'
 
@@ -44,30 +46,29 @@ def get_data(file_path):
         urls_list = [line.strip() for line in file.readlines()]
 
     urls_count = len(urls_list)
-
-    s = requests.Session()
     result_data = []
 
-    for url in enumerate(urls_list[:100]):
-        response = s.get(url=url[1], headers=headers)
-        soup = BeautifulSoup(response.text, 'lxml')
+    with requests.Session() as session:
+        for url in enumerate(urls_list[:100]):
+            response = session.get(url=url[1], headers=headers)
+            soup = BeautifulSoup(response.text, 'lxml')
 
-        article_title = soup.find('div', class_='post-content').find('h1', class_='title').text.strip()
-        article_date = soup.find('div', class_='post').find('div', class_='tile-views').text.strip()
-        article_img = f"https://hi-tech.news{soup.find('div', class_='post-media-full').find('img').get('src')}"
-        article_text = soup.find('div', class_='the-excerpt').text.strip().replace('\n', '')
+            article_title = soup.find('div', class_='post-content').find('h1', class_='title').text.strip()
+            article_date = soup.find('div', class_='post').find('div', class_='tile-views').text.strip()
+            article_img = f"https://hi-tech.news{soup.find('div', class_='post-media-full').find('img').get('src')}"
+            article_text = soup.find('div', class_='the-excerpt').text.strip().replace('\n', '')
 
-        result_data.append(
-            {
-                'original_url': url[1],
-                'article_title': article_title,
-                'article_date': article_date,
-                'article_img': article_img,
-                'article_text': article_text
-            }
-        )
-        # print(f'{article_title}\n{article_date}\n{article_img}\n{10*"#"}')
-        print(f'Обработал {url[0] + 1}/{urls_count}')
+            result_data.append(
+                {
+                    'original_url': url[1],
+                    'article_title': article_title,
+                    'article_date': article_date,
+                    'article_img': article_img,
+                    'article_text': article_text
+                }
+            )
+            # print(f'{article_title}\n{article_date}\n{article_img}\n{10*"#"}')
+            print(f'Обработал {url[0] + 1}/{urls_count}')
 
     with open('result.json', 'w') as file:
         json.dump(result_data, file, indent=4, ensure_ascii=False)
